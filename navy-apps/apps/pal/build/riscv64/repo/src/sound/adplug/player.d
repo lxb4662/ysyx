a333e51098fcb26f93f5df4a39d849d64e4fdd0b
22050518-li xinbao <466277875@qq.com>
2022-11-24 21:30:29 +0800

    new file:   navy-apps/apps/menu/build/menu-riscv64 	new file:   navy-apps/apps/menu/build/native/menu.d

---
+++ navy-apps/apps/pal/build/riscv64/repo/src/sound/adplug/player.d
@@ -0,0 +1,5 @@
+/home/lixinbao/ysyx-workbench/navy-apps/apps/pal/build/riscv64/repo/src/sound/adplug/player.o: \
+ /home/lixinbao/ysyx-workbench/navy-apps/apps/pal/repo/src/sound/adplug/player.cpp \
+ /home/lixinbao/ysyx-workbench/navy-apps/apps/pal/repo/src/sound/adplug/player.h \
+ /home/lixinbao/ysyx-workbench/navy-apps/apps/pal/repo/src/sound/adplug/opl.h \
+ repo/include/opltypes.h
