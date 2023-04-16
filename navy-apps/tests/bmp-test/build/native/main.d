1b992de5ad9bf789c9b2c61cbd87059708dff4dc
22050518-li xinbao <466277875@qq.com>
2023-04-16 15:32:55 +0800

    new file:   navy-apps/Makefile

---
+++ navy-apps/tests/bmp-test/build/native/main.d
@@ -0,0 +1,4 @@
+/home/lixinbao/ysyx-workbench/navy-apps/tests/bmp-test/build/native/main.o: \
+ /home/lixinbao/ysyx-workbench/navy-apps/tests/bmp-test/main.c \
+ /home/lixinbao/ysyx-workbench/navy-apps/libs/libndl/include/NDL.h \
+ /home/lixinbao/ysyx-workbench/navy-apps/libs/libbmp/include/BMP.h
