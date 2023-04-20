6be1714d576f4fcd82b04c0a8d7f8d667fc3d4b7
22050518-li xinbao <466277875@qq.com>
2023-04-20 15:41:25 +0800

    deleted:    navy-apps/libs/libSDL_image/build/libSDL_image-riscv64.a

--- navy-apps/libs/libminiSDL/build/native/src/timer.d
+++
@@ -1,4 +0,0 @@
-/home/lixinbao/ysyx-workbench/navy-apps/libs/libminiSDL/build/native/src/timer.o: \
- /home/lixinbao/ysyx-workbench/navy-apps/libs/libminiSDL/src/timer.c \
- /home/lixinbao/ysyx-workbench/navy-apps/libs/libndl/include/NDL.h \
- /home/lixinbao/ysyx-workbench/navy-apps/libs/libminiSDL/include/sdl-timer.h
